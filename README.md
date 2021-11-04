6335a2b6e0cc93ca24be0708d880877b5a1d9830
Subdev514 <82996094+Subdev514@users.noreply.github.com>
2021-11-04 18:20:33 +0545

    Add files via upload

---
+++ README.md
@@ -0,0 +1,7 @@
+Rquirements
+- install twilio module by doing `pip install twilio` in cmd
+- motivational.txts file also needed in the same folder as the main.py
+- Create a twilio account and you can get a number for you to send messages from
+
+ALSO NOTE that you can only send sms to verified numbers
+Replace the sting in main.py saying you to put accountsid and authtoken with your sid and token
---
+++ main.py
@@ -0,0 +1,21 @@
+from twilio.rest import Client
+import random
+# Importing module that are needed
+
+account_sid = 'YOUR TWILIO ACOUNT.SID HERE'
+auth_token = 'YOUR TWILIO AUTH_TOKEN HERE'
+client = Client(account_sid, auth_token)
+# Your accountsid and token goes here
+
+with open('motivational_texts.txt') as file:
+    result = file.readlines()
+# Storing every line of motivatiobal_texts.txt in result
+
+sms = str((' '.join(random.choices(result))))
+# Formating sting
+
+message = client.messages.create(
+    to="REPLACE THIS WITH THE NBUMBER U WANT TO SEND MSGS TO", 
+    from_="REPLACE THIS NUMBER WITH THE NUMBER U WERE GIVEN FROM TWILIO",
+    body=sms)
+#Sending the msg
